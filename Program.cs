7b31726a2e8a0b98f43c5dac9a162263e1962af2
Ceren <acerendogan3@gmail.com>
2026-08-06 16:29:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("1 - Metni kendim gireceğim.");
+Console.WriteLine("2 - metin.txt dosyasını okuyor");
+Console.Write("Seçiminiz:");
+string secim = Console.ReadLine() ??"";
+string metin = "";
+if (secim == "1")
+{
+    Console.Write("Metni giriniz:");
+    metin = Console.ReadLine() ?? "";
+}
+else if (secim == "2")
+{
+    metin = File.ReadAllText("metin.txt");
+}
+int karakterSayisi = metin.Length;
+string temizMetin = metin.ToLower();
+temizMetin= temizMetin.Replace(",", "");
+temizMetin= temizMetin.Replace(".", "");
+temizMetin= temizMetin.Replace("!", "");
+temizMetin= temizMetin.Replace("?", "");
+string []kelimeler = temizMetin.Split(
+    new char[] { ' ', '\t', '\n', '\r' }, 
+    StringSplitOptions.RemoveEmptyEntries);
+int kelimeSayisi = kelimeler.Length;
+
+string enUzunKelime = "";
+foreach (string kelime in kelimeler)
+{
+    if (kelime.Length > enUzunKelime.Length)
+    {
+        enUzunKelime = kelime;
+    }
+}
+
+string enKisaKelime = kelimeler[0];
+foreach (string kelime in kelimeler)
+{
+    if (kelime.Length < enKisaKelime.Length)
+    {
+        enKisaKelime = kelime;
+    }
+}
+
+Console.WriteLine($"Karakter sayısı: {karakterSayisi}");
+Console.WriteLine($"Kelime sayısı: {kelimeSayisi}");
+Console.WriteLine($"En uzun kelime: {enUzunKelime}");   
+Console.WriteLine($"En kısa kelime: {enKisaKelime}");
+
+Array.Sort(kelimeler);
+Console.WriteLine("Kelimeler alfabetik sırayla:");
+foreach (string kelime in kelimeler) 
+{
+    Console.WriteLine(kelime);
+}
---
+++ README.md
@@ -0,0 +1,59 @@
+# Task01 - Text Analyzer
+
+## 📌 Proje Amacı
+
+Bu proje, .NET ve C# öğrenme sürecinin ilk görevi olarak geliştirilmiştir.
+
+Amaç; kullanıcıdan alınan veya bir metin dosyasından okunan metni analiz ederek temel C# yapılarını öğrenmektir.
+
+---
+
+## 🎯 Görev
+
+Program iki farklı şekilde metin alacaktır.
+
+1. Kullanıcının konsoldan metin girmesi
+2. `metin.txt` dosyasından metin okunması
+
+Daha sonra alınan metin üzerinde çeşitli analizler yapılacaktır.
+
+---
+
+## 🛠️ Kullanılan Teknolojiler
+
+- C#
+- .NET 10
+- Console Application
+- Visual Studio Code
+
+---
+
+## 📚 Öğrenilen Konular
+
+- Console.WriteLine()
+- Console.ReadLine()
+- if / else
+- switch
+- string
+- Dosya okuma
+- Döngüler
+- List
+- Dictionary
+
+---
+
+## 🚀 Yapılacak Özellikler
+
+- [ ] Kullanıcıdan metin alma
+- [ ] metin.txt dosyasından okuma
+- [ ] Karakter sayısını bulma
+- [ ] Kelime sayısını bulma
+- [ ] En uzun kelimeyi bulma
+- [ ] En çok tekrar eden kelimeyi bulma
+- [ ] Kelime arama
+
+---
+
+## 👨‍💻 Geliştirici
+
+Ayşe Ceren Doğan
